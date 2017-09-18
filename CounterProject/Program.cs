2760416bf46d59f5ae80af4f24249e2c7cc49625
Bob <bobevans547@gmail.com>
2017-09-18 13:38:26 -0400

    CounterProject

--- CounterProject/Program.cs
+++ CounterProject/Program.cs
@@ -8,8 +8,14 @@ namespace CounterProject
 {
     class Program
     {
+        void Run() {
+            for (int idx=0, nbr=1; idx<10; idx++,nbr++) {
+                Console.WriteLine($"IDX is{idx}; Number is {nbr}");
+            }
+        }
         static void Main(string[] args)
         {
+            new Program().Run();
         }
     }
 }
